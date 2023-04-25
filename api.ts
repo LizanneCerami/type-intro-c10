65a9547c06ab015a3fa1489f436b502833549255
Lizanne Cerami <lizanne.cerami@gmail.com>
2023-04-25 16:09:25 -0400

    added express and cors

---
+++ api.ts
@@ -0,0 +1,14 @@
+import cors from 'cors'
+import express, { Request, Response } from 'express'
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+app.get('/', (req: Request, res: Response) => {
+  res.send("welcome to typescript")
+})
+app.listen(3000, () => {
+  console.log("listening on PORT 3000")
+})
